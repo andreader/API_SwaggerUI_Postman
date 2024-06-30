c4bb927058752f53277df668a7484e92b8460814
Андрей <79538233369@ya.ru>
2024-06-30 12:16:15 +0500

    Initial

---
+++ src/main/java/pro/sky/api_swaggerui_postman/ApiSwaggerUiPostmanApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.api_swaggerui_postman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSwaggerUiPostmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSwaggerUiPostmanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/api_swaggerui_postman/ApiSwaggerUiPostmanApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.api_swaggerui_postman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSwaggerUiPostmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
